59fc77319d693075a053cf1137cf4fbd0b464f9f
Mani1358 <mani621998@gmail.com>
2022-11-28 00:05:50 +0530

    Added:welcome message added

---
+++ EmpAttendance.java
@@ -0,0 +1,9 @@
+public class EmpAttendance {
+public static void main( String[] args ) {
+if (Math.random()<0.5){
+System.out.println("Employee is Absent");
+}else{
+System.out.println("Employee is Present");
+    }
+  }
+}
